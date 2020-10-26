51a0c7adb5b90a9c270e4929a2ccb55699383210
ruth <ruth1998bigman@gmail.com>
2020-10-26 21:22:32 +0800

    error stash

--- experiment2/leNet-5.py
+++ experiment2/leNet-5.py
@@ -11,39 +11,47 @@ class LeNetConfig(Config):
         self.batch_size = 64
         self.num_epochs = 40
 
+class ConvandPool(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size,\
+                 stride=1, padding=2, pool_size=2, pool_striker=2 ):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(pool_size, pool_striker)
+
+    def forward(self,x):
+        x = self.conv(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        return x
+
 class LeNet(nn.Module):
     def __init__(self, LeNetConfig):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1)
-        self.pool1 = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(8, 8, kernel_size=3, stride=1, padding=1)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        # self.fc1 = nn.Linear(int(8*28*28), 28*28)
-        self.fc1 = nn.Linear(7 * 7 * 8, 28 * 28)
-        self.fc2 = nn.Linear(28*28, 10)
+
+        self.CP1 = ConvandPool(1, 6, 5, 1, 2, 2, 2)
+        self.CP2 = ConvandPool(6,16, 5, 1, 2, 2,2)
+        self.CP3 = ConvandPool(16, 120, 5, 1, 2, 2, 2)
+
+        self.fc1 = nn.Linear(3 * 3 * 120, 100)
+        self.fc2 = nn.Linear(100, 10)
 
         # 设置 data tpye of model weight
         self.to(LeNetConfig.device)
 
 
     def forward(self, x):
+        x = self.CP1(x)
+        x = self.CP2(x)
+        x = self.CP3(x)
+        # print(x.shape)
+        x = torch.flatten(x, 1)
 
-        conv1 = self.conv1(x)
-        conv1 = self.pool1(conv1)
-        conv2 = self.conv2(conv1)
-        conv2 = self.pool1(conv2)
-
-        # flat the input from the dim 1
-        # print(conv2.shape)
-        flattedx = torch.flatten(conv2, 1)
-        # print(flattedx.shape)
-        # fc layers
-        fc1 = self.fc1(flattedx)
+        fc1 = self.fc1(x)
         fc1 = relu(fc1)
         fc2 = self.fc2(fc1)
-        fc2 = relu(fc2)
+        fc2 = softmax(fc2, 1)
         return fc2
-
 
 
 import torchvision
